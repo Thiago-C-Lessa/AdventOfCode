c21d996af4b2ccd79e8b5e75823be84ba8733049
Thiago-C-Lessa <115082857+Thiago-C-Lessa@users.noreply.github.com>
2025-02-22 21:46:16 -0300

    package

---
+++ go/days/day1.go
@@ -0,0 +1,7 @@
+package day1
+
+import "fmt"
+
+func olaola() {
+	fmt.Println("ximira")
+}
---
+++ go/main.go
@@ -0,0 +1,11 @@
+package main
+
+import(
+	"fmt"
+	"github.com/Thiago-C-Lessa/adventofcode/days/day1"
+)
+
+func main(){
+	fmt.Println("ola")
+
+}
